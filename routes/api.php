b71033d0c84ce100dbc6898c467f00896708a071
nao <tirtyemrzk@gmail.com>
2026-08-02 20:58:17 +0900

    feat: API実装 #26

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiBookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::get('/books', [ApiBookController::class, 'index']);
+    Route::get('/books/{book}', [ApiBookController::class, 'show']);
+    Route::post('/books', [ApiBookController::class, 'store']);
+    Route::put('/books/{book}', [ApiBookController::class, 'update']);
+    Route::delete('/books/{book}', [ApiBookController::class, 'destroy']);
 });
