b72ec608b78672374aa50c650b0a5f895d182e9f
nao <tirtyemrzk@gmail.com>
2026-08-05 22:30:48 +0900

    feat: UnitTest実装 #28

---
+++ database/factories/BookUserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Book;
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookUser>
+ */
+class BookUserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => Book::factory(),
+            'user_id' => User::factory(),
+            //
+        ];
+    }
+}
